b1116b8cbdf6d3e160937b05a68dfa771221119f
MadeleyneSilva <silvau.madeleyne@gmail.com>
2023-07-11 01:29:48 -0400

    sección 5 hasta servicio publico

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({
@@ -7,29 +8,34 @@ import { Character } from '../interfaces/character.interface';
 export class DbzService {
 
   public characters: Character[] = [{
+    id: uuid(),
     name: 'Krillin',
     power: 1000
   },
   {
+    id: uuid(),
     name: 'Goku',
     power: 9500
   },
   {
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }];
 
   onNewCharacter( character: Character ): void{
-    // console.log('Main Page');
-    // console.log(character);
-    this.characters.push(character);
+
+    const newCharacter: Character = { id: uuid(), ...character    }; //operador spread
+
+    this.characters.push(newCharacter);
   }
 
-  onDeleteCharacter(id: number): void{
-    this.characters.splice(id, 1);
+  // onDeleteCharacter(id: number): void{
+  //   this.characters.splice(id, 1);
+  // }
+  deleteCharacterById(id: string){
+    this.characters = this.characters.filter(character => character.id !== id);
   }
-
-  constructor() { }
 
 
 }
